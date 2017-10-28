06a9d09a5d3fa19b04aef4ce9d4082604333ba82
Fela Maslen <felamaslen@gmail.com>
2017-10-28 18:24:25 +0100

    List of services in menu

--- src/nav/nav-box-services.js
+++ src/nav/nav-box-services.js
@@ -1,4 +1,27 @@
 import { h } from 'preact';
 
-export default () => <span>Services</span>;
+export default () => <div className="nav-box-services">
+    <h3>Services</h3>
+    <ul className="nav-dropdown">
+        <li>
+            {'Gateway '}
+            <em>{'(DHCP via SH3, ISP: Virgin Media)'}</em>
+        </li>
+        <li>
+            {'Router '}
+            <em>{'(IPv4 and IPv6)'}</em>
+        </li>
+        <li>
+            {'Firewall '}
+            <em>{'(iptables)'}</em>
+        </li>
+        <li>
+            {'DNS server '}
+            <em>{'(bind9)'}</em>
+        </li>
+        <li>{'File storage '}<em>{'(zfs)'}</em></li>
+        <li>{'Dokku container host'}</li>
+        <li>{'Media streaming '}<em>{'(Plex)'}</em></li>
+    </ul>
+</div>;
 
