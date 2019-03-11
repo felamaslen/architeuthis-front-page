09c160b103a97a891bea8b305d4b1879c757fe84
Fela Maslen <fela.maslen@mubaloo.com>
2019-03-11 19:25:42 +0000

    Upgraded node version and only require dotenv on development

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const dotenv = require('dotenv');
 if (process.env.NODE_ENV === 'development') {
-    dotenv.config();
+    require('dotenv').config(); // eslint-disable-line global-require
 }
 
 const app = require('./app');
