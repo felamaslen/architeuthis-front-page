e3f6aa0dc62cb5b6b5d347a073462dbcf9283d04
Fela Maslen <felamaslen@gmail.com>
2021-06-20 19:12:55 +0100

    fix: UPS command try/catch

--- app/ups.js
+++ app/ups.js
@@ -7,30 +7,31 @@ const APCACCESS_CACHE_FILE = path.join(__dirname, './.apcaccess.cache');
 const TIMEOUT_COMMAND = 5000;
 
 function processUPSValues(config, { data, ...rest }) {
-    const propsRaw = data.split('\n')
-        .map(line => line.match(/^([^\s]+).*?:\s*([^\s].*?)\s*$/))
-        .filter(match => match && match.length > 1)
-        .map(match => [match[1], match[2]])
+    const propsRaw = data
+        .split('\n')
+        .map((line) => line.match(/^([^\s]+).*?:\s*([^\s].*?)\s*$/))
+        .filter((match) => match && match.length > 1)
+        .map((match) => [match[1], match[2]])
         .reduce((obj, [key, prop]) => ({ ...obj, [key]: prop }), {});
 
     const upsProps = [
         {
             key: 'date',
             rawKey: 'DATE',
-            proc: value => new Date(value).getTime()
+            proc: (value) => new Date(value).getTime()
         },
         {
             key: 'model',
             rawKey: 'MODEL',
-            proc: value => value
+            proc: (value) => value
         },
         {
             key: 'load',
             rawKey: 'LOADPCT',
-            proc: value => {
+            proc: (value) => {
                 const [, loadPercent] = value.match(/^([0-9]+)/);
 
-                const loadWatts = loadPercent * config.common.constants.upsCapacity / 100;
+                const loadWatts = (loadPercent * config.common.constants.upsCapacity) / 100;
 
                 return `${Number(loadPercent).toFixed(1)}% (${loadWatts.toFixed(1)}W)`;
             }
@@ -38,66 +39,63 @@ function processUPSValues(config, { data, ...rest }) {
         {
             key: 'charge',
             rawKey: 'BCHARGE',
-            proc: value => `${(+value.match(/^([0-9]+)/)[1]).toFixed(0)}%`
+            proc: (value) => `${(+value.match(/^([0-9]+)/)[1]).toFixed(0)}%`
         },
         {
             key: 'backupTime',
             rawKey: 'TIMELEFT',
-            proc: value => `${(+value.match(/^([^\s]+)/)[1]).toFixed(0)} min`
+            proc: (value) => `${(+value.match(/^([^\s]+)/)[1]).toFixed(0)} min`
         },
         {
             key: 'transfers',
             rawKey: 'NUMXFERS',
-            proc: value => Math.round(+value)
+            proc: (value) => Math.round(+value)
         },
         {
             key: 'lastPowerFailure',
             rawKey: 'XONBATT',
-            proc: value => new Date(value).getTime()
+            proc: (value) => new Date(value).getTime()
         },
         {
             key: 'timeOnBattery',
             rawKey: 'CUMONBATT',
-            proc: value => value
+            proc: (value) => value
         }
     ];
 
-    const props = upsProps
-        .reduce((obj, { key, rawKey, proc }) => {
-            try {
-                const value = proc(propsRaw[rawKey]);
+    const props = upsProps.reduce((obj, { key, rawKey, proc }) => {
+        try {
+            const value = proc(propsRaw[rawKey]);
 
-                obj[key] = value === null
-                    ? '<not found>'
-                    : value;
-            } catch {
-                obj[key] = '<error>';
-            }
+            obj[key] = value === null ? '<not found>' : value;
+        } catch {
+            obj[key] = '<error>';
+        }
 
-            return obj;
-
-        }, {});
+        return obj;
+    }, {});
 
     return { props, ...rest };
 }
 
 function getUPSCacheExists(config, logger) {
     return new Promise((resolve, reject) => {
-        fs.open(APCACCESS_CACHE_FILE, 'r', err => {
+        fs.open(APCACCESS_CACHE_FILE, 'r', (err) => {
             if (err) {
                 return resolve(false);
             }
 
             return fs.stat(APCACCESS_CACHE_FILE, (statErr, stats) => {
                 if (statErr) {
-                    logger.error('Couldn\'t stat UPS cache file: %s', statErr.message);
+                    logger.error("Couldn't stat UPS cache file: %s", statErr.message);
 
                     return reject(statErr);
                 }
 
                 const mtime = Number(stats.mtime);
 
-                const cacheNotStale = Date.now() - mtime < config.common.constants.apcaccessCacheMaxAge;
+                const cacheNotStale =
+                    Date.now() - mtime < config.common.constants.apcaccessCacheMaxAge;
 
                 return resolve(cacheNotStale);
             });
@@ -107,25 +105,7 @@ function getUPSCacheExists(config, logger) {
 
 function runUPSStatusCommand(logger) {
     return new Promise((resolve, reject) => {
-        let output = '';
-
-        const args = (process.env.UPS_COMMAND || 'apcaccess').split(' ');
-
-        const command = args.shift();
-
-        const apc = spawn(command, args);
-
-        const timeout = setTimeout(() => {
-            logger.error('UPS command timed out');
-
-            reject(new Error('Timeout on apcaccess'));
-        }, TIMEOUT_COMMAND);
-
-        apc.stdout.on('data', data => {
-            output += data;
-        });
-
-        const rejectWithError = err => {
+        const rejectWithError = (err) => {
             logger.error('Error running UPS command: %s', err.message);
 
             clearTimeout(timeout);
@@ -133,14 +113,36 @@ function runUPSStatusCommand(logger) {
             reject(err);
         };
 
-        apc.on('error', rejectWithError);
-        apc.stderr.on('data', rejectWithError);
+        const timeout = setTimeout(() => {
+            logger.error('UPS command timed out');
 
-        apc.on('close', () => {
-            clearTimeout(timeout);
+            reject(new Error('Timeout on apcaccess'));
+        }, TIMEOUT_COMMAND);
 
-            resolve(output);
-        });
+        let output = '';
+
+        const args = (process.env.UPS_COMMAND || 'apcaccess').split(' ');
+
+        const command = args.shift();
+
+        try {
+            const apc = spawn(command, args);
+
+            apc.stdout.on('data', (data) => {
+                output += data;
+            });
+
+            apc.on('error', rejectWithError);
+            apc.stderr.on('data', rejectWithError);
+
+            apc.on('close', () => {
+                clearTimeout(timeout);
+
+                resolve(output);
+            });
+        } catch (err) {
+            reject(err);
+        }
     });
 }
 
@@ -167,7 +169,7 @@ function getUPSStatusRaw(config, logger) {
         try {
             const data = await runUPSStatusCommand(logger);
 
-            return fs.writeFile(APCACCESS_CACHE_FILE, data, err => {
+            return fs.writeFile(APCACCESS_CACHE_FILE, data, (err) => {
                 if (err) {
                     logger.error('Error writing to UPS cache file: %s', err.message);
 
@@ -197,4 +199,3 @@ function getUPSStatus(config, logger) {
 }
 
 module.exports = { getUPSStatus };
-
