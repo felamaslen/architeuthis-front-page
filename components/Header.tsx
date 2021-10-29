2697a6e94d7c79fbfa866d9bcda67f7668517826
Fela Maslen <felamaslen@gmail.com>
2021-10-29 20:59:59 +0100

    feat: server hostname

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ type Props = {
 
 export const Header: React.FC<Props> = ({ clientHostname }) => (
     <header>
+        <h2>{process.env.NEXT_PUBLIC_SERVER_HOSTNAME}</h2>
         <h5>Viewed from {clientHostname}</h5>
     </header>
 );
