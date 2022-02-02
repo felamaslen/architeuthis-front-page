23ead876ced5453166a479c6c13f4123bf6bc3cd
Fela Maslen <felamaslen@gmail.com>
2022-02-02 15:01:28 +0000

    fix: add env variables to client

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_TITLE: process.env.NEXT_PUBLIC_TITLE,
+        NEXT_SERVER_HOSTNAME: process.env.NEXT_SERVER_HOSTNAME,
+        NEXT_INSTALLATION_TIME: process.env.NEXT_INSTALLATION_TIME,
+    },
 };
